c138cfd92d51c96c0b3554ff94baf7d4be56a0b9
Hemanth Bandi <bandi.hemanth145@gmail.com>
2023-08-25 18:02:53 +0530

    Added extra functionalities to TextUtils

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -2,18 +2,20 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
-  const [text, setText] = useState("Enter the text here....");
-  const handleOnClick = () =>{
+  const [text, setText] = useState("");
+  const handleOnUpClick = () =>{
     setText(text.toUpperCase());
   }
+  const handleOnLoClick = () =>{
+    setText(text.toLowerCase());
+  }
   const handleOnChange = (event) =>{
-    console.log("On change count")
     setText(event.target.value);
   }
   return (
     <div>
       
-      <div className="mb-3" >
+      <div className="container mb-3" >
         <label htmlFor="OpArea" className="form-label">
         <h3>{props.heading}</h3>
         </label>
@@ -24,7 +26,13 @@ export default function Textform(props) {
           rows="8"
           onChange={handleOnChange}
         ></textarea>
-        <button className='btn btn-primary mt-3' type='submit' onClick={handleOnClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary my-3' type='submit' onClick={handleOnUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary m-3' type='submit' onClick={handleOnLoClick}>Convert to Lowercase</button>
+      
+      </div>
+      <div className="container mb-3" >
+        <h3><b>Text Summary: </b></h3>
+        <b>{text.split(" ").length}</b> Words and <b>{text.length}</b> Characters.
       </div>
     </div>
   );
